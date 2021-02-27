33ef7d10ab6ed283d43a6b4a2a98fb036ba85440
Atli Arnarsson <atli.nikulas@gmail.com>
2021-02-27 13:08:03 -0800

    added button styling back

--- src/components/MainScreen.vue
+++ src/components/MainScreen.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="main">
-        <h1>FLASH CHAT MOTHAFUCKAS</h1>
+        <h1>FLASH CHAT </h1>
         <button v-on:click="goToVideoPage" >Join a Meeting</button>
     </div>
 </template>
@@ -23,4 +23,23 @@
 </script>
 
 <style scoped>
+button {
+    margin: 5px;
+    font-size: 30px;
+    position: relative;
+    border-radius: 40px;
+    border: 0px solid #000;
+    padding: 20px;
+    padding-left: 70px;
+    padding-right: 70px;
+    transition: padding 1s;
+    outline: none;
+}
+
+button:hover {
+    animation-fill-mode: forwards;
+    padding-left: 170px;
+    padding-right: 170px;
+    transition: padding 1s;
+}
 </style>
