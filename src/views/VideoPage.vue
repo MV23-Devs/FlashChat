6802f45d0c42ee5d4b65f3221ee0aef7bc3fc8f7
Jacob Ismael <jacob.ismael@gmail.com>
2021-02-27 15:37:45 -0800

    stash

--- src/views/VideoPage.vue
+++ src/views/VideoPage.vue
@@ -1,7 +1,6 @@
 
 <template> 
     <div id="VideoPage">
-        <DisplayCard :clue="DisplayClue" :answer="DisplayAnswer"/>
         <VideoChat msg="Flash Study"/>
         <Flashcard/>
     </div>
@@ -10,23 +9,14 @@
 
 
 <script> 
-    import DisplayCard from '../components/DisplayCard.vue'
     import VideoChat from '../components/VideoChat.vue'
     import Flashcard from '../components/Flashcard.vue'
 
     export default {
     name: 'App',
     components: {
-        DisplayCard,
         VideoChat,
         Flashcard
-    },
-    data() {
-        return {
-        messages: ["lol", "yeeyee", "haw", "shamone"],
-        DisplayClue: 'yeet',
-        DisplayAnswer: 'yeet2',
-        }
     }
     }    
 
