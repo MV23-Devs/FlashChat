3f7ec5224847831e9d7641540ef882e4ed5e7acf
saarangbond <48871052+saarangbond@users.noreply.github.com>
2021-02-28 01:07:56 -0800

    minor work on meeting controls

--- src/components/VideoChat.vue
+++ src/components/VideoChat.vue
@@ -38,17 +38,26 @@
           >Join</el-button
         >
         <br />
-        <el-button
+        <!-- <el-button
           type="primary"
           @click="leaveEvent"
           plain
           :disabled="!disableJoin"
           class="Buttons1"
           id="LeaveBtn"
-          >Leave</el-button
-        >
+          >Leave
+        </el-button> -->
       </div>
     </div>
+    <el-button
+      type="primary"
+      @click="leaveEvent"
+      plain
+      :disabled="!disableJoin"
+      class="Buttons1"
+      id="LeaveBtn"
+      >Leave
+    </el-button>
     <div class="agora-view">
       <div class="agora-video">
         <StreamPlayer
@@ -136,10 +145,12 @@ export default {
           log("join channel error", err);
         });
       this.disableJoin = true;
+      console.log("disableJoin = true")
     },
     leaveEvent() {
       this.$router.push("/");
       this.disableJoin = false;
+      console.log("disableJoin = false")
       this.rtc
         .leaveChannel()
         .then(() => {
@@ -280,6 +291,8 @@ export default {
 
 #LeaveBtn {
   color: deepskyblue;
+  display: block;
+  margin: auto;
 }
 </style>
 
